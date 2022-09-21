134da3888cdf6d46465a610ae06f9691447579fb
MykolasMi <mykolas.miskinisss@gmail.com>
2022-09-21 22:02:25 +0300

    add decideTurn function to switch between players turns

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 const boardDiv = document.querySelector('.gameboard');
 
 const gameboard = (() => {
+    let player1Turn = true;
+    let mark;
 
     const board = ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
 
@@ -14,22 +16,56 @@ const gameboard = (() => {
             if (square.textContent=="X" || square.textContent=="O") {
                 return
             }
-            board.splice([i], 1, "X")
+            else if (player1Turn == true) {
+                mark = "X";
+            }
+            else {
+                 mark = "O";
+            }
+
+            board.splice([i], 1, mark)
             square.textContent = board[i];
 
+            decideTurn();
             console.log(board);
+            console.log(mark);
+            console.log(player1Turn)
         })
 
         console.log(boardDiv)
         }
     }
 
+    const decideTurn = () => {
+        if (player1Turn == true) {
+            player1Turn = false;
+        }
+        else {
+            player1Turn = true;
+        }
+
+        console.log("work")
+    }
+
     return {board, display}
 })()
 
-const player = (() => {
-    
+const player1 = (() => {
+    //win 
+
+
+    /*let player1Turn = true;
+    const place = () => {
+        if (player1Turn == true) {
+            player1Turn = false;
+        }
+        else {
+            player1Turn = true;
+        }
+    }
+    return{place}*/
 })()
+
 
 gameboard.display()
 

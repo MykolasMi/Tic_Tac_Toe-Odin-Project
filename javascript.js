1caa9049e6ec228170c3489fc4ca0e3d920c624d
MykolasMi <mykolas.miskinisss@gmail.com>
2022-11-05 20:17:00 +0200

    add working js check for winning, add basic css styling, add container and upper text in html

--- javascript.js
+++ javascript.js
@@ -6,10 +6,69 @@ const gameboard = (() => {
 
     const board = ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
 
+    const checkWin = (mark) => {
+        console.log("checkwin");
+        for(let i=0; i<3; i++) {
+            //row check
+            if(board[i] == board[i+1] && board[i+1] == board[i+2]) {
+                console.log("EVEN_ROW_TRUE");
+                if (mark == "X") {
+                    //player1.win;
+                    console.log("X WIN")
+                    break
+                }
+                if (mark == "O") {
+                    //player2.win
+                    console.log("O WIN")
+                    break
+                }
+            }
+            //column check
+            if(board[i] == board[i+3] && board[i+3] == board[i+6]) {
+                console.log("EVEN_COLUMN_TRUE");
+                if (mark == "X") {
+                    //player1.win;
+                    console.log("X WIN")
+                    break
+                }
+                if (mark == "O") {
+                    //player2.win
+                    console.log("O WIN")
+                    break
+                }
+            }
+
+        }
+        //diagonal check
+        if(board[0] == board[4] && board[4] == board[8]) {
+            console.log("EVEN_LTR_DIAGONAL_TRUE");
+            if (mark == "X") {
+                //player1.win;
+                console.log("X WIN")
+            }
+            if (mark == "O") {
+                //player2.win
+                console.log("O WIN")
+            }
+        }
+        if(board[2] == board[4] && board[4] == board[8]) {
+            console.log("EVEN_RTL_DIAGONAL_TRUE");
+            if (mark == "X") {
+                //player1.win;
+                console.log("X WIN")
+            }
+            if (mark == "O") {
+                //player2.win
+                console.log("O WIN")
+            }
+        }
+    }
+
     const display = () => {
         for (let i=0; i<board.length; i++) {
         const square = document.createElement('div');
         square.textContent = board[i];
+        square.classList.add("square");
         boardDiv.appendChild(square);
         
         square.addEventListener('click', function(e) {
@@ -26,6 +85,7 @@ const gameboard = (() => {
             board.splice([i], 1, mark)
             square.textContent = board[i];
 
+            checkWin(mark);
             decideTurn();
             console.log(board);
             console.log(mark);
